a5ac79938e8129549b943ed06cbbe9e51db71583
Taewon Kim <twnkm14@gmail.com>
2024-05-25 18:42:28 -0700

    update until 9.1 Github Authentication

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Carron Market Reloaded
+
 This project is for studying.
+
+# Tailwind CSS Plugins
+
+## @tailwindcss/forms
+
+```sh
+npm install -D @tailwindcss/forms
+```
+
+# Prisma commands
+
+```sh
+npx prisma init
+```
+
+```sh
+npx prisma migrate dev
+```
+
+```sh
+npx prisma studio
+```
+
+# References
+
+[tailwindcss-forms][tailwindcss-forms]
+[password-generator][password-generator]
+
+[tailwindcss-forms]: https://github.com/tailwindlabs/tailwindcss-forms
+[password-generator]: https://1password.com/password-generator/
--- app/signin/actions.ts
+++ app/signin/actions.ts
@@ -1,31 +1,82 @@
 "use server";
 
 import {
-    PASSWORD_MIN_LENGTH,
-    PASSWORD_REGEX,
-    PASSWORD_REGEX_ERROR,
+  PASSWORD_MIN_LENGTH,
+  PASSWORD_REGEX,
+  PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return Boolean(user);
+};
 
 const formSchema = z.object({
-    email: z.string().email().toLowerCase(),
-    password: z
-        .string({ required_error: "Password is required" })
-        .min(PASSWORD_MIN_LENGTH)
-        .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+  email: z
+    .string()
+    .email()
+    .toLowerCase()
+    .refine(checkEmailExists, "An account with this email does not exists."),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(PASSWORD_MIN_LENGTH),
+  // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
 });
 
 export async function signin(prevState: any, formData: FormData) {
-    const data = {
-        email: formData.get("email"),
-        password: formData.get("password"),
-    };
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
 
-    const result = formSchema.safeParse(data);
+  const result = await formSchema.spa(data);
 
-    if (!result.success) {
-        return result.error.flatten();
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    // if the user is found, check password hash
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+
+    const isMatchPassword = await bcrypt.compare(
+      result.data.password,
+      user!.password ?? ""
+    );
+
+    if (isMatchPassword) {
+      const session = await getSession();
+      session.id = user!.id;
+      await session.save();
+      redirect("/profile");
     } else {
-        console.log(result.data);
+      return {
+        fieldErrors: {
+          password: ["Wrong password."],
+          email: [],
+        },
+      };
     }
+    // sign the user in
+    // redirect to "/profile"
+  }
 }
