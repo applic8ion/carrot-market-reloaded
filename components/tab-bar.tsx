0bf2b103224621deb53bf42feaefe616fb4a96da
Taewon Kim <twnkm14@gmail.com>
2024-05-28 22:36:29 -0700

    update until 10.1 Tab Bar

--- components/tab-bar.tsx
+++ components/tab-bar.tsx
@@ -1,16 +1,28 @@
 "use client";
 
-import { HomeIcon as SolidHomeIcon } from "@heroicons/react/24/solid";
-import { HomeIcon as OutlineHomeIcon } from "@heroicons/react/24/outline";
+import {
+  HomeIcon as SolidHomeIcon,
+  NewspaperIcon as SolidNewspaperIcon,
+  ChatBubbleOvalLeftEllipsisIcon as SolidChatBubbleOvalLeftEllipsisIcon,
+  VideoCameraIcon as SolidVideoCameraIcon,
+  UserIcon as SolidUserIcon,
+} from "@heroicons/react/24/solid";
+import {
+  HomeIcon as OutlineHomeIcon,
+  NewspaperIcon as OutlineNewspaperIcon,
+  ChatBubbleOvalLeftEllipsisIcon as OutlineChatBubbleOvalLeftEllipsisIcon,
+  VideoCameraIcon as OutlineVideoCameraIcon,
+  UserIcon as OutlineUserIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function TabBar() {
   const pathname = usePathname();
   return (
-    <div className="fixed bottom-0 w-full mx-auto max-w-screen-md grid grid-cols-5 border-neutral-600 border-t px-5 py-3">
-      <Link href="/check" className="flex flex-col items-center gap-px">
-        {pathname === "/check" ? (
+    <div className="fixed bottom-0 w-full mx-auto max-w-screen-md grid grid-cols-5 border-neutral-600 border-t px-5 py-3 *:text-white">
+      <Link href="/checks" className="flex flex-col items-center gap-px">
+        {pathname === "/checks" ? (
           <SolidHomeIcon className="w-7 h-7" />
         ) : (
           <OutlineHomeIcon className="w-7 h-7" />
@@ -19,33 +31,33 @@ export default function TabBar() {
       </Link>
       <Link href="/life" className="flex flex-col items-center gap-px">
         {pathname === "/life" ? (
-          <SolidHomeIcon className="w-7 h-7" />
+          <SolidNewspaperIcon className="w-7 h-7" />
         ) : (
-          <OutlineHomeIcon className="w-7 h-7" />
+          <OutlineNewspaperIcon className="w-7 h-7" />
         )}
         <span>Life</span>
       </Link>
       <Link href="/chat" className="flex flex-col items-center gap-px">
         {pathname === "/chat" ? (
-          <SolidHomeIcon className="w-7 h-7" />
+          <SolidChatBubbleOvalLeftEllipsisIcon className="w-7 h-7" />
         ) : (
-          <OutlineHomeIcon className="w-7 h-7" />
+          <OutlineChatBubbleOvalLeftEllipsisIcon className="w-7 h-7" />
         )}
         <span>Chat</span>
       </Link>
       <Link href="/live" className="flex flex-col items-center gap-px">
         {pathname === "/live" ? (
-          <SolidHomeIcon className="w-7 h-7" />
+          <SolidVideoCameraIcon className="w-7 h-7" />
         ) : (
-          <OutlineHomeIcon className="w-7 h-7" />
+          <OutlineVideoCameraIcon className="w-7 h-7" />
         )}
         <span>Live</span>
       </Link>
       <Link href="/profile" className="flex flex-col items-center gap-px">
         {pathname === "/profile" ? (
-          <SolidHomeIcon className="w-7 h-7" />
+          <SolidUserIcon className="w-7 h-7" />
         ) : (
-          <OutlineHomeIcon className="w-7 h-7" />
+          <OutlineUserIcon className="w-7 h-7" />
         )}
         <span>Profile</span>
       </Link>
